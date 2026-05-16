ed0c44879425aaac9e7b174028ff4660e9e9ee86
Aapo Hakuni <aapohakuni1@gmail.com>
2026-05-16 12:41:13 +0300

    maini

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-# testing emt
-# kulliprint("Hello world")
-print("Hello kulli")
-for i in range(5):
-    print("jarvis anna mulle " + str(i+1) + " banaania")
+from dataReading import MCAPReader
+from detection import YOLODetector
+# from visualizer import Visualizer
+
+def main():
+    print("Käynnistetään järjestelmä...")
+    
+    # Alustetaan osat
+    lukija = MCAPReader(data_dir="data/mcap_files", kohdetiedosto="dualtarget.mcap")
+    aivot = YOLODetector()
+    
+    kuva_laskuri = 0
+    
+    # Lypsetään mcap-tiedostoa kuva kerrallaan
+    for syvyyskuva in lukija.lue_kuvat_generaattorina():
+        kuva_laskuri += 1
+        
+        # 1. Annetaan kuva Aivoille, saadaan takaisin lista koordinaatteja
+        esteet = aivot.etsi_esteet(syvyyskuva)
+        
+        # Printataan tulokset testin vuoksi konsoliin
+        if esteet:
+            print(f"Ruutu {kuva_laskuri}: Löytyi {len(esteet)} kohdetta! Esimerkki: {esteet[0]}")
+            
+        if kuva_laskuri >= 100:
+            break
+
+if __name__ == "__main__":
+    main()
