7801a4f22cccd7eac50e34761b5af7947e8cffdc
Aapo Hakuni <aapohakuni1@gmail.com>
2026-05-16 19:11:15 +0300

    vis päivitys jetson

--- visualization.py
+++ visualization.py
@@ -94,6 +94,5 @@ class Visualizer:
         # Piirretään tyylikäs valkoinen reunus tutkan ympärille
         cv2.rectangle(naytettava_kuva, (x_alku, y_alku), (x_loppu, y_loppu), (255, 255, 255), 1)
 
-        # Näytetään tulos
-        cv2.imshow(self.ikkunan_nimi, naytettava_kuva)
-        return cv2.waitKey(10) & 0xFF
+        # Palautetaan tulos
+        return naytettava_kuva
