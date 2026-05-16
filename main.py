9325ecefb748293f83a9fe5ba197765befa390a3
Aapo Hakuni <aapohakuni1@gmail.com>
2026-05-16 13:08:27 +0300

    maini taas

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-# Kuvitteellinen main.py arkkitehtuurin havainnollistamiseksi
 from dataReading import MCAPReader
 from detection import YOLODetector
+from visualization import Visualizer
+import cv2
 
-lukija = MCAPReader(data_dir="data/mcap_files", kohdetiedosto="dualtarget.mcap")
-aivot = YOLODetector()
+def main():
+    print("Käynnistetään järjestelmä...")
+    
+    # 1. Alustetaan kaikki 3 moduulia
+    lukija = MCAPReader(data_dir="data/mcap_files", kohdetiedosto="dualtarget.mcap")
+    aivot = YOLODetector()
+    silmat = Visualizer()
+    
+    kuva_laskuri = 0
+    
+    # 2. Aloitetaan datan lypsäminen generaattorilla
+    for raakakuva in lukija.lue_kuvat_generaattorina():
+        kuva_laskuri += 1
+        
+        # Vaihe A: Aivot etsivät koordinaatit
+        esteet = aivot.etsi_esteet(raakakuva)
+        
+        # Vaihe B: Silmät piirtävät tulokset ruudulle
+        nappain = silmat.piirra_hud(raakakuva, esteet)
+        
+        # Jos käyttäjä painaa 'q', ohjelma sammuu turvallisesti
+        if nappain == ord('q'):
+            print("Käyttäjä keskeytti lennon.")
+            break
 
-for kuva in lukija.lue_kuvat_generaattorina():
-    esteet = aivot.etsi_esteet(kuva)
-    # Tämän jälkeen listassa 'esteet' on siististi kaikki ruudulta löytyneet laatikot!
+    # Lopuksi siivotaan ikkunat pois
+    cv2.destroyAllWindows()
+    print(f"Ajo suoritettu. Käsitelty {kuva_laskuri} ruutua.")
+
+if __name__ == "__main__":
+    main()
