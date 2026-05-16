e995fae4e0939319e284079488035f66571cf938
Aapo Hakuni <aapohakuni1@gmail.com>
2026-05-16 13:06:20 +0300

    maini

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
+# Kuvitteellinen main.py arkkitehtuurin havainnollistamiseksi
 from dataReading import MCAPReader
 from detection import YOLODetector
-# from visualizer import Visualizer
 
-def main():
-    print("Käynnistetään järjestelmä...")
-    
-    # Alustetaan osat
-    lukija = MCAPReader(data_dir="data/mcap_files", kohdetiedosto="dualtarget.mcap")
-    aivot = YOLODetector()
-    
-    kuva_laskuri = 0
-    
-    # Lypsetään mcap-tiedostoa kuva kerrallaan
-    for syvyyskuva in lukija.lue_kuvat_generaattorina():
-        kuva_laskuri += 1
-        
-        # 1. Annetaan kuva Aivoille, saadaan takaisin lista koordinaatteja
-        esteet = aivot.etsi_esteet(syvyyskuva)
-        
-        # Printataan tulokset testin vuoksi konsoliin
-        if esteet:
-            print(f"Ruutu {kuva_laskuri}: Löytyi {len(esteet)} kohdetta! Esimerkki: {esteet[0]}")
-            
-        if kuva_laskuri >= 100:
-            break
+lukija = MCAPReader(data_dir="data/mcap_files", kohdetiedosto="dualtarget.mcap")
+aivot = YOLODetector()
 
-if __name__ == "__main__":
-    main()
+for kuva in lukija.lue_kuvat_generaattorina():
+    esteet = aivot.etsi_esteet(kuva)
+    # Tämän jälkeen listassa 'esteet' on siististi kaikki ruudulta löytyneet laatikot!
